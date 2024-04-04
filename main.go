b31f198ec832de4129dd48f6faecff99519a8a39
amitjalui <amitjalui5@gmail.com>
2024-04-05 01:46:58 +0530

    createTodo, deleteTodos

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func fetchTodos(w http.ResponseWriter, r *http.Request){
 		})
 		return
 	}
-	
+
 	todoList := []todo{}
 
 	for _, t := range todos{
@@ -78,6 +78,65 @@ func fetchTodos(w http.ResponseWriter, r *http.Request){
 	}
 	rnd.JSON(w, http.StatusOK, renderer.M{
 		"data": todoList,
+	})
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request){
+	var t todo
+
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		rnd.JSON(w, http.StatusProcessing, err)
+		return
+	}
+
+	if t.Title == ""{
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "The title is required",
+		})
+		return
+	}
+
+	tm := todoModel{
+		ID: bson.NewObjectId(),
+		Title: t.Title,
+		Completed: false,
+		CreatedAt: time.Now(),
+	}
+
+	if err := db.C(collectionName).Insert(&tm); err != nil{
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed to save todo",
+			"error": err,
+		})
+		return
+	}
+
+	rnd.JSON(w, http.StatusCreated, renderer.M{
+		"message": "todo created successfully",
+		"todo_id": tm.ID.Hex(),
+	})
+}
+
+func deleteTodos(w http.ResponseWriter, r *http.Request){
+	id := strings.TrimSpace(chi.URLParam(r, "id"))
+
+	if !bson.IsObjectIdHex(id){
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "The id is invalid",
+		})
+		return
+	}
+
+	if err := db.C(collectionName).RemoveId(bson.ObjectIdHex(id)); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed to delete todo",
+			"error": err,
+		})
+		return
+	}
+
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"message": "Todo deleted successfully",
 	})
 }
 
